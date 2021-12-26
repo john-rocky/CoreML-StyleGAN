af0d1604b3a40878dcf888ef53f2dfa10eb290d6
john-rocky <john-rocky@hubot.com>
2021-12-26 11:20:06 +0900

    Initial Commit

---
+++ CoreML-StyleGAN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreML-StyleGAN
+//
+//  Created by 間嶋大輔 on 2021/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
